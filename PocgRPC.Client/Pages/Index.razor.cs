32cf967bd01d1881f481728e40f25ebf43c1df84
Sébastien Bernard <niou128@hotmail.com>
2020-03-10 09:34:36 +0100

    first commit

---
+++ PocgRPC.Client/Pages/Index.razor.cs
@@ -0,0 +1,23 @@
+﻿using Grpc.Net.Client;
+using Microsoft.AspNetCore.Components;
+using PocgRPC.Client.Protos;
+using System.Threading.Tasks;
+
+namespace PocgRPC.Client.Pages
+{
+    public class IndexBase : ComponentBase
+    {
+        protected string Greeting;
+
+        protected async Task SayHello()
+        {
+            var channel = GrpcChannel.ForAddress("https://localhost:5001");
+            var client = new Greeter.GreeterClient(channel);
+
+            var reply = await client.SayHelloAsync(new HelloRequest { Name = "Blazor gRPC Client" });
+            Greeting = reply.Message;
+
+            await channel.ShutdownAsync();
+        }
+    }
+}
